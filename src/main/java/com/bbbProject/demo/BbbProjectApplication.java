f6f5081eb3733b3e0369ef9854acb72dda5f8709
admin <admin@admins-air>
2021-09-29 10:21:34 -0500

    Initial Commit

---
+++ src/main/java/com/bbbProject/demo/BbbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bbbProject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbbProjectApplication.class, args);
+	}
+
+}
